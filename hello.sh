330a66fecd7db4c2d6d346056b276db837ae8dab
Jbrincklow <brinckofinnovation@gmail.com>
2017-08-25 06:53:30 -0700

    More learning

--- hello.sh
+++ hello.sh
@@ -2,3 +2,4 @@ Third message written.
 Hello! This is a different message now.
 I am learning about how to make changes on the file.
 
+Now I am learning about the difference between the index and working directory
