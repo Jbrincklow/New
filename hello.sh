78eece0daa3ce8b322fda9a8a4feb6c7cbc70466
Jbrincklow <brinckofinnovation@gmail.com>
2017-08-25 07:00:09 -0700

    convert string to uppercase

--- hello.sh
+++ hello.sh
@@ -2,4 +2,5 @@ Third message written.
 Hello! This is a different message now.
 I am learning about how to make changes on the file.
 
-Now I am learning about the difference between the index and working directory
+Now I am learning about the difference between the index and working directoysdd
+Now I am leraning about changing branches and committing changes to new branches.
